989e770e278aa45b4c2d705466bc6153504a59d9
Pranav Rao <pranavrao55@gmail.com>
2025-06-11 02:38:31 -0700

    Add route for reading one student

--- api/students.js
+++ api/students.js
@@ -9,17 +9,40 @@ const router = express.Router();
 import Student from "../schemas/Student.js";
 import User from "../schemas/User.js";
 
-router.get("/read/:id", (req, res) => {
-    console.log(req.params.id);
-    res.json({error: false, msg: "Read a student"});
+router.get("/read/:id", async (req, res) => {
+    try {
+        const data = await Student.where({ id_short: req.params.id }).findOne();
+        res.json({
+            error: false, 
+            msg: "Read one student", 
+            data: {
+                id_short: data.id_short,
+                fname: data.fname,
+                lname: data.lname,
+                active: data.active,
+            }
+        });
+    }
+    catch (err) {
+        console.log("Error getting all students");
+        res.json({error: true, msg: "Error!"});
+    }
 });
 
 router.get("/read", async (req, res) => {
     try {
-        const data = Student.find().sort({ lname: -1 });
-        res.json({error: false, msg: "Read all students", data: data});
+        const data = await Student.find().sort({ lname: -1 });
+        const data_res = data.map(datum => {
+            return {
+                id_short: datum.id_short,
+                fname: datum.fname,
+                lname: datum.lname,
+                active: datum.active,
+            }
+        });
+        res.json({ error: false, msg: "Read all students", data: data_res});
     }
-    catch {
+    catch (err) {
         console.log("Error getting all students");
         res.json({error: true, msg: "Error!"});
     }
